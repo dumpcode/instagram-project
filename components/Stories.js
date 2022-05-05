aa31bada34ba669fc9c677944a9a193b9b9b52f1
dumpedcode <censoredvillain@gmail.com>
2022-05-05 13:10:09 -0400

    adding margin to left and right sections of Stories and Post

--- components/Stories.js
+++ components/Stories.js
@@ -18,7 +18,7 @@ function Stories() {
 
     return (
     <div className='flex space-x-2 p-6 bg-white 
-    mt-8 border-gray-200 border rounded-sm
+    mt-8 mx-2.5 border-gray-200 border rounded-sm
     overflow-x-scroll 
     scrollbar-thin scrollbar-thumb-black'>
         {session && (
